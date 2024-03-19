ec33f80ffda9c75296b27d8286431b1cf7fa73c4
Jesse Hines <hinesjr@ornl.gov>
2024-03-19 14:04:50 -0400

    Tweak to comment

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ docker build -t registry.apps.marble.ccs.ornl.gov/stf218-app/exadigit-simulation
 docker build -t registry.apps.marble.ccs.ornl.gov/stf218-app/exadigit-simulation-server-simulation-job:latest -f Dockerfile.simulation . &&
 docker push registry.apps.marble.ccs.ornl.gov/stf218-app/exadigit-simulation-server:latest &&
 docker push registry.apps.marble.ccs.ornl.gov/stf218-app/exadigit-simulation-server-simulation-job:latest &&
-# # Scale down so pod gets recreated and uses new image
+# Scale down so pod gets recreated and uses new image
 oc --namespace stf218-app scale deploy -l app=exadigit-simulation-server --replicas=0
 
 oc apply -f deployment.yaml
