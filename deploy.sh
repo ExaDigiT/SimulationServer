1d578ee2edc4f52963c013444f1c5278e1013576
Jesse Hines <hinesjr@ornl.gov>
2024-04-23 10:24:43 -0400

    Fixes to stage/prod deploy
    If you don't update the image tag sometimes the job image gets cached and doesn't update,
    so use the full sha name of an image

--- deploy.sh
+++ deploy.sh
@@ -7,23 +7,23 @@ cd "$SCRIPT_DIR"
 ENV=$1
 if [ "$ENV" != "prod" ] && [ "$ENV" != "stage" ]; then
     echo 'You need to pass either "prod" or "stage"'
+    exit
 fi
-SERVER_IMAGE="registry.apps.marble.ccs.ornl.gov/stf218-app/exadigit-simulation-server"
-JOB_IMAGE="registry.apps.marble.ccs.ornl.gov/stf218-app/exadigit-simulation-server-simulation-job"
+SERVER_IMAGE_STREAM="registry.apps.marble.ccs.ornl.gov/stf218-app/exadigit-simulation-server"
+JOB_IMAGE_STREAM="registry.apps.marble.ccs.ornl.gov/stf218-app/exadigit-simulation-server-simulation-job"
 
-docker build -t $SERVER_IMAGE:latest-$ENV -f Dockerfile.server .
-docker build -t $JOB_IMAGE:latest-$ENV -f Dockerfile.simulation .
-docker push $SERVER_IMAGE:latest-$ENV
-docker push $JOB_IMAGE:latest-$ENV
-if [[ "$ENV" = "prod" ]]; then # Alias latest-prod to latest
-    docker tag $SERVER_IMAGE:latest-prod $SERVER_IMAGE:latest
-    docker tag $JOB_IMAGE:latest-prod $JOB_IMAGE:latest
-    docker push $SERVER_IMAGE:latest
-    docker push $JOB_IMAGE:latest
-fi
+docker build -t $SERVER_IMAGE_STREAM:latest -f Dockerfile.server .
+docker build -t $JOB_IMAGE_STREAM:latest -f Dockerfile.simulation .
+docker push $SERVER_IMAGE_STREAM:latest
+docker push $JOB_IMAGE_STREAM:latest
+
+SERVER_IMAGE=$(docker inspect --format='{{index .RepoDigests 0}}' $SERVER_IMAGE_STREAM:latest)
+JOB_IMAGE=$(docker inspect --format='{{index .RepoDigests 0}}' $JOB_IMAGE_STREAM:latest)
 
 # Scale down so pod gets recreated and uses new image. Allow error if pod doesn't exist
 oc --namespace stf218-app scale deploy -l env=$ENV,app=exadigit-simulation-server --replicas=0 || true 
 
 # Process template and apply
-oc process -f ./deployment.yaml -o yaml --param=ENV=$ENV | oc apply -f -
+oc process -f ./deployment.yaml -o yaml \
+    --param=ENV=$ENV --param=SERVER_IMAGE="$SERVER_IMAGE" --param=JOB_IMAGE="$JOB_IMAGE" \
+    | oc apply -f -
