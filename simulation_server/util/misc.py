ea60e6f65964fcf75943f47cf97f4d278c6628cc
Jesse Hines <hinesjr@ornl.gov>
2024-10-04 10:55:49 -0400

    Add nest_dict helper

--- simulation_server/util/misc.py
+++ simulation_server/util/misc.py
@@ -1,7 +1,7 @@
-from typing import TypeVar
+from typing import TypeVar, Any
 from collections.abc import Iterable
 from datetime import timedelta
-import json
+import json, re
 
 from pydantic import TypeAdapter
 
@@ -26,3 +26,46 @@ def pick(d: dict[T, U], keys: Iterable[T]) -> dict[T, U]:
 def omit(d: dict[T, U], keys: Iterable[T]) -> dict[T, U]:
     key_set = set(keys)
     return {k: v for k, v in d.items() if k not in key_set}
+
+
+def parse_path(path: str):
+    """ Parse simple json paths like "a.b[1]['d']" """
+    part_pat = r"""^(\w+)|\.(\w+)|\['([^']+)'\]|\["([^"]+)"\]|\[(\d+)\]"""
+
+    if not re.fullmatch(f"({part_pat})+", path):
+        raise ValueError(f"Invalid path {repr(path)}")
+    
+    def parse_match(m: re.Match):
+        # get index first (and only) group with data
+        i = next(i + 1 for i, g in enumerate(m.groups()) if g)
+        # Parse int only for `[0]` syntax
+        return int(m[i]) if i == 5 else m[i]
+
+    return [parse_match(m) for m in re.finditer(part_pat, path)]
+
+
+def nest_dict(data: dict[str, Any]):
+    """
+    Takes a flat dictionary and "nests" it based on key names.
+
+    keys containing dots or `[]` index syntax will be put under subdicts of the same path. E.g.
+    `{"a": 1, "b.c": 2, "b.d": 3, "b['e'].f": 4}` wil return
+    `{"a": 1, "b": {"c": 2, "d": 3, "e": {"f": 4}}}`
+    """
+    out = {}
+    
+    for k, v in data.items():
+        path = parse_path(k)
+        sub = out
+        for part in path[:-1]:
+            if part not in sub:
+                sub[part] = {}
+            sub = sub[part]
+            if not isinstance(sub, dict):
+                raise ValueError(f"{repr(k)} conflicts with another key")
+        if path[-1] in sub:
+            raise ValueError(f"{repr(k)} conflicts with another key")
+        sub[path[-1]] = v
+
+    return out
+
