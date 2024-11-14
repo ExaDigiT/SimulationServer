5b35c015fc17bcbad8ae2e089db4aec736e9b57d
Jesse Hines <hinesjr@ornl.gov>
2024-11-14 16:27:03 -0500

    Add ingests for other system data

---
+++ scripts/fetch.sh
@@ -0,0 +1,24 @@
+set -e
+
+mkdir downloads
+cd downloads
+
+# lassen
+git clone https://github.com/LLNL/LAST/ lassen-repo
+cd lassen-repo
+git lfs pull
+cd ..
+mkdir lassen
+mv lassen-repo/Lassen-Supercomputer-Job-Dataset/*.csv lassen
+rm -rf lassen-repo
+python3 ../scripts/preprocess_lassen.py lassen
+
+# marconi
+wget https://zenodo.org/api/records/10127767/files-archive -O marconi100.zip
+unzip marconi100.zip -d marconi100
+rm marconi100.zip
+
+# fugaku
+wget https://zenodo.org/api/records/11467483/files-archive -O fugaku.zip
+unzip fugaku.zip -d fugaku 
+rm fugaku/*.csv
---
+++ scripts/preprocess_lassen.py
@@ -0,0 +1,15 @@
+
+from pathlib import Path
+import pandas as pd
+import sys
+
+path = Path(sys.argv[1])
+
+node_history_df = pd.read_csv(path / 'final_csm_allocation_node_history.csv')
+allocation_history_df = pd.read_csv(path / 'final_csm_allocation_history_hashed.csv', usecols=['allocation_id', 'begin_time'])
+
+allocation_to_time = {r.allocation_id: r.begin_time for r in allocation_history_df.itertuples()}
+
+node_history_df['job_begin_time'] = node_history_df['allocation_id'].apply(lambda id: allocation_to_time[id])
+
+node_history_df.to_csv(path / 'final_csm_allocation_node_history_with_time.csv')
