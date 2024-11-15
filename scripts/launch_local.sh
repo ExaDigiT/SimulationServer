717ff27d97843ac522bdc3b413b60249b1bb9ddb
Jesse Hines <hinesjr@ornl.gov>
2024-11-15 09:40:57 -0500

    Fix launch local

--- scripts/launch_local.sh
+++ scripts/launch_local.sh
@@ -2,8 +2,8 @@
 # Launch local version
 set -e # Exit if any commmand fails
 
-SCRIPT_DIR=$(realpath $(dirname "${BASH_SOURCE[0]}"))
-cd "$SCRIPT_DIR"
+BASE_DIR=$(realpath $(dirname "${BASH_SOURCE[0]}")/..)
+cd "$BASE_DIR"
 
 docker build -t exadigit-simulation-server:latest -f Dockerfile.server .
 # docker build -t exadigit-simulation-server-simulation-job:latest -f Dockerfile.simulation .
