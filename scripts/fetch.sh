74963e40506c9b146491559b63c8535d2a22c6bc
Jesse Hines <hinesjr@ornl.gov>
2025-10-16 17:31:25 -0400

    Update fetch script

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -2,24 +2,12 @@
 set -e
 
 mkdir data
-cd data
 
-# lassen
-git clone https://github.com/LLNL/LAST/ lassen-repo
-cd lassen-repo
-git lfs pull
-cd ..
-mkdir lassen
-mv lassen-repo/Lassen-Supercomputer-Job-Dataset/*.csv lassen
-rm -rf lassen-repo
-python3 ../scripts/preprocess_lassen.py lassen
+raps download --system lassen --dest ./data/lassen
+mv ./data/lassen/Lassen-Supercomputer-Job-Dataset/* ./data/lassen
+rm -rf ./data/lassen/Lassen-Supercomputer-Job-Dataset
+python3 ./scripts/preprocess_lassen.py ./data/lassen
 
-# marconi
-wget https://zenodo.org/api/records/10127767/files-archive -O marconi100.zip
-unzip marconi100.zip -d marconi100
-rm marconi100.zip
-
-# fugaku
-wget https://zenodo.org/api/records/11467483/files-archive -O fugaku.zip
-unzip fugaku.zip -d fugaku 
-rm fugaku/*.csv
+raps download --system marconi100 --dest ./data/marconi100
+raps download --system fugaku --dest ./data/fugaku
+raps download --system adastraMI250 --dest ./data/adastraMI250
